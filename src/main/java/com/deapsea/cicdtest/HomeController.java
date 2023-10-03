48baf9b4dc6ed7e89b163cd6e3207af283db537f
dlrbcnvk <dlrbcnvk@naver.com>
2023-10-03 16:47:39 +0900

    java code updated

--- src/main/java/com/deapsea/cicdtest/HomeController.java
+++ src/main/java/com/deapsea/cicdtest/HomeController.java
@@ -1,5 +1,7 @@
 package com.deapsea.cicdtest;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +11,10 @@ public class HomeController {
     @GetMapping("/")
     public String index() {
         return "index";
+    }
+
+    @GetMapping("/test")
+    public ResponseEntity<String> test() {
+        return new ResponseEntity<>("TEST~~~~~~", HttpStatus.OK);
     }
 }
