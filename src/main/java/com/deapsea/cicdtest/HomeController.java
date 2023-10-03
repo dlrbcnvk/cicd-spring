b92396c3fc145cda48fb6092b7e0506ef8be2ba6
dlrbcnvk <dlrbcnvk@naver.com>
2023-10-03 23:57:23 +0900

    add submodule -> build...?

--- src/main/java/com/deapsea/cicdtest/HomeController.java
+++ src/main/java/com/deapsea/cicdtest/HomeController.java
@@ -1,15 +1,23 @@
 package com.deapsea.cicdtest;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class HomeController {
+
+    @Value("${kakao.apiKey}")
+    private String apiKey;
+
 
     @GetMapping("/")
     public String index() {
+        log.info("kakao.apiKey={}", apiKey);
         return "index";
     }
 
